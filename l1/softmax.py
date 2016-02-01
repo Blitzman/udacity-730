20c66a1a1a456fcf7d88dc1be12ad71942f47ceb
Albert García <albert.garcia.ua@gmail.com>
2016-02-01 17:30:47 +0100

    add L1 momentum, learning rate decay and SGD considerations

--- l1/softmax.py
+++ l1/softmax.py
@@ -15,5 +15,6 @@ import matplotlib.pyplot as plt
 x = np.arange(-2.0, 6.0, 0.1)
 scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
 
+plt.figure(figsize=(30,10))
 plt.plot(x, softmax(scores).T, linewidth=2)
 plt.show()
